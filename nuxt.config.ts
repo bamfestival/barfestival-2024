08b6a1428a23efd4bbf7ba192565b8e56927330a
Ronald Punt <ronald.punt@mac.com>
2023-12-06 18:29:38 +0100

    n wel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
         
     public: {
-        apiBase: process.env.API_BASE_URL || "https://api.bamfestival.nl/",
+        apiBase: process.env.API_BASE_URL || "https://api.bamfestival.nl",
         sponsorenApi: (process.env.API_BASE_URL || "https://api.bamfestival.nl") + '/jsonapi/node/sponsors?filter[status][value]=1&sort=-field_weight,title&include=field_image&jsonapi_include=1&fields[node--sponsors]=title,field_link,sticky,promote,field_image',
     }
 },
