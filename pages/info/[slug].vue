240da913cae68ad21613e42c028d368964303c29
Ronald Punt <ronald.punt@bamfestival.nl>
2023-12-10 23:23:15 +0100

    files bi pages

--- pages/info/[slug].vue
+++ pages/info/[slug].vue
@@ -1,34 +1,44 @@
 <template>
-  <nuxt-img class="border-5 border-white shadow-xl m-3" :src="`${ config.public.apiBase }${image.attributes.uri.url }`"  format="webp" :alt="`Logo ${ title  }, sponsor van BAM! Festival Hengelo (Ov.)`"></nuxt-img>
+  <nuxt-img v-if="image" class="border-5 border-white shadow-xl m-3" :src="image"  format="webp" :alt="`Logo ${ title  }, sponsor van BAM! Festival Hengelo (Ov.)`"></nuxt-img>
 	<h1>{{ title }}</h1>
-	<div class="p-4 m-2 border-gray-600 border-1" v-html="body"></div>
-  <pre>{{ result[0] }}</pre>
+  <div>{{ slug }}</div>
+	<div class="p-4 m-2 border-gray-600 border-1" v-html="body" v-if="body"></div><hr>
+  <ul>
+    <li v-for="item in result.data[0].relationships.field_files.data ">{{  item.id }} {{ item.meta.description }} {{ (result.included.find(x => x.id === item.id).attributes.uri.url ) }}</li>
+  </ul>
+  <pre>{{  result.included }}</pre>
+
 </template>
 <script setup lang="ts">
-  let title = '';
-  let body = '';
-  
-  const config = useRuntimeConfig();
+      let title = '';
+      let body = '';
+      let image = '';
   const route = useRoute();
+  const config = useRuntimeConfig();
   const slug = (route.params.slug).toLowerCase();
-  const pagesApi = 'https://api.bamfestival.nl/jsonapi/node/page?json_include=1&include=field_image,field_images,field_files,field_tags&filter[field_slug][value]='+slug;
-  const { data: result }  = await $fetch(pagesApi);
-  const { data: image }  = await $fetch('https://api.bamfestival.nl/jsonapi/file/file/'+result[0].relationships.field_image.data.id);
-  console.log(image.attributes.uri.url);
-  watchEffect(() => {
-  const slug = route.params.slug;
-
-  if (result[0]) {
-    // Update the reactive page object
-    title = result[0].attributes.title;
-    body = result[0].attributes.body.value
-    
-  } else {
-    // Handle not found (you might want to set default values or show an error)
-    title = 'Page Not Found';
-    body = 'Sorry, the requested page was not found.';
-  }
-});
+  const pagesApi = config.public.apiBase+'/jsonapi/node/page?json_include=1&include=field_image,field_images,field_files,field_tags&filter[field_slug][value]='+slug;
+  
+  const result = await $fetch(pagesApi);
+  
+  if(result.data[0])
+    {
+        console.log('Fetch successful:', result.data[0].relationships.field_image.data.id);
+        const imageApi = config.public.apiBase+'/jsonapi/file/file/'+result.data[0].relationships.field_image.data.id;
+        const imageresult = await $fetch(imageApi); 
+        image = config.public.apiBase+imageresult.data.attributes.uri.url;
+        title = result.data[0].attributes.title;
+        body = result.data[0].attributes.body.value
+        const filesx = await result.data[0].relationships.field_files.data;
+        console.log(filesx);
+        
+     
+        
+    } else {
+      console.log('Fetch unsuccesful');
+        let title = 'Sorry, deze pagina bestaat niet';
+        let body = '';
+        let image = '';
+    }
   
 
 </script>
